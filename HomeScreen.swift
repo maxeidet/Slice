a39546ea634cfdd39f948bcf55e1a0d756dfd649
maxeidet <max.eidet@gmail.com>
2024-11-26 20:45:02 +0100

    Initial Commit

---
+++ HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  Slice
+//
+//  Created by Max Eidet on 2024-11-26.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
