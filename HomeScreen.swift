2be55b18e1f9507c4fe5845e1731dba9fa928905
maxeidet <max.eidet@gmail.com>
2024-11-26 20:47:51 +0100

    Test

--- HomeScreen.swift
+++ HomeScreen.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct HomeScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Hello")
+                .font(.title)
+            Text("Max")
+                .font(.title3)
+        }
+       
     }
 }
 
