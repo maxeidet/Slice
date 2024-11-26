7d1f30351ce7577c35011a525676314166f5a2be
maxeidet <max.eidet@gmail.com>
2024-11-26 20:25:29 +0100

    Initial Commit

---
+++ Slice/SliceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliceApp.swift
+//  Slice
+//
+//  Created by Max Eidet on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct SliceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
